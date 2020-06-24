39fb332a82bbca15c8918077ce584e5f469dd18f
toddmark <toddmark0315@gmail.com>
2020-06-24 17:48:10 +0800

    p150

--- movie.php
+++ movie.php
@@ -1,6 +1,17 @@
 <?php
-$db = mysqli_connect('localhost', 'root', 'root');
-mysqli_select_db($db, 'moviesite') or die(mysqli_error($db));
+  $db = mysqli_connect('localhost', 'root', 'root');
+  mysqli_select_db($db, 'moviesite') or die(mysqli_error($db));
+  if($_GET['action'] == 'edit') {
+    $query = 'SELECT * FROM movie WHERE movie_id=' . $_GET['id'];
+    $result = mysqli_query($db, $query);
+    extract(mysqli_fetch_assoc($result));
+  } else {
+    $movie_name = '';
+    $movie_type = 0;
+    $movie_year = date('Y');
+    $movie_leadactor = 0;
+    $movie_director = 0;
+  }
 ?>
 
 <!DOCTYPE html>
@@ -8,14 +19,16 @@ mysqli_select_db($db, 'moviesite') or die(mysqli_error($db));
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Add Movie</title>
+  <title><?php
+    echo ucfirst($_GET['action']);
+  ?> Movie</title>
 </head>
 <body>
-  <form action="commit.php?action=add&type=movie" method="post">
+  <form action="commit.php?action=<?php echo $_GET['action']; ?>&type=movie" method="post">
     <table>
       <tr>
         <td>Movie Name</td>
-        <td><input type="text" name="movie_name" /></td>
+        <td><input type="text" name="movie_name" value="<?php echo $movie_name; ?>"/></td>
       </tr>
       <tr>
         <td>Movie Type</td>
@@ -26,7 +39,11 @@ mysqli_select_db($db, 'moviesite') or die(mysqli_error($db));
               $result = mysqli_query($db, $query);
               while($row = mysqli_fetch_assoc($result)) {
                 extract($row);
-                echo '<option value="' . $movietype_id .'">' . $movietype_label . '</option>';
+                if ($movie_type == $movietype_id) {
+                  echo '<option selected="selected" value="' . $movietype_id .'">' . $movietype_label . '</option>';
+                } else {
+                  echo '<option value="' . $movietype_id .'">' . $movietype_label . '</option>';
+                }
               }
             ?>
           </select>
